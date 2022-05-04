15dd7a32ea3bda5eb32f2096552c11a0a8e9f584
DeagoBrowne <89912823+DeagoBrowne@users.noreply.github.com>
2022-05-04 21:57:53 +0100

    added comments for easier navigation

--- src/PageLayout.js
+++ src/PageLayout.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const PageLayout = ({ babyNames }) => {
 
-
+  // button colour settings
   const fColor = {
     backgroundColor: 'gold'
   }
@@ -11,10 +11,12 @@ const PageLayout = ({ babyNames }) => {
     color: 'gold'
   }
 
+  // creating states for filter
   const [search, setSearch] = useState('')
 
   const [filteredNames, setFilteredNames] = useState(babyNames)
 
+  // filter component
   const filter = (e) => {
 
     const keyword = e.target.value;
@@ -31,6 +33,7 @@ const PageLayout = ({ babyNames }) => {
     setSearch(keyword)
   }
 
+  // sorting names into ascending alphabetical order
   babyNames.sort((a, b) => {
     if (a.name < b.name) {
       return -1;
@@ -41,8 +44,10 @@ const PageLayout = ({ babyNames }) => {
     return 0;
   })
 
+  // creating page...
   return (
     <div>
+      {/* container handling searchbar */}
       <div className="searchComponent">
         <p>Name Search</p>
         <input className="searchbar"
@@ -52,16 +57,25 @@ const PageLayout = ({ babyNames }) => {
           placeholder="Type name..."
         />
       </div>
+
+      {/* container handling array of buttons */}
       <div className="name-selection">
+
+        {/* checking if name is in the array */}
         {filteredNames && filteredNames.length > 0 ? (
           filteredNames.map((obj) => {
+
+            // defining variable used to choose colour settings
             const sex = obj.sex;
+
+            // creating buttons
             return (
               <button key={obj.id}
                 style={sex === "f" ? fColor : mColor}>
                 {obj.name}
               </button>);
           })) : (
+          // message will appear if search is not part of array
           <h1>No results found..</h1>
         )}
       </div>
